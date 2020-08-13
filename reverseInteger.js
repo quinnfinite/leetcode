956ada11fdb4fe78c5a7f38e2394fc3b12f2a4b8
quinnfinite <quinn.royston@gmail.com>
2020-08-13 15:08:27 -0700

    began OICE on reverseInteger

---
+++ reverseInteger.js
@@ -0,0 +1,34 @@
+// https://leetcode.com/problems/reverse-integer/
+
+`
+OICE:
+
+O: Integer
+I: Integer
+
+C: integer range is +- 2^31
+E: If integer overflows, return 0;
+`
+
+`
+Diagram:
+I: 123
+
+123 % 10 -> 3
+new: 3
+123 - 3
+120
+120 % 100 -> 20
+20 % 10 -> 2
+new: 32
+100-20 -> 100
+
+
+
+`
+
+`
+Pseudocode: 
+
+`
+
