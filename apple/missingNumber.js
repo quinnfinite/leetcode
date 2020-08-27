8f03c19b1a499cc34ff14f49c221fdfe984610cf
quinnfinite <quinn.royston@gmail.com>
2020-08-27 13:50:44 -0700

    solution 2 for missing Number

--- apple/missingNumber.js
+++ apple/missingNumber.js
@@ -1,4 +1,5 @@
 /*
+https://leetcode.com/submissions/detail/387271124/?from=/explore/interview/card/apple/344/array-and-strings/3114/
 OICE:
 
 O: Number - Missing
@@ -20,13 +21,19 @@ iterate over the array
 return sum
 */
 
-//Solution
-var missingNumber = function(nums) {
-  var sum = nums.length*(nums.length+1)/2
-  nums.forEach((num) => sum -= num)
-  return sum  
-};
+//Solution 1
+// var missingNumber = function(nums) {
+//   var sum = nums.length*(nums.length+1)/2
+//   nums.forEach((num) => sum -= num)
+//   return sum  
+// };
 
+//Solution 2 - Reduce - Faster Runtime
+var missingNumber = function(nums) {
+    return nums.reduce((accumulator, currentValue) => {
+        return accumulator - currentValue
+    }, nums.length*(nums.length+1)/2)
+  };
 
 //Tests
 console.log(missingNumber([3,0,1]))
