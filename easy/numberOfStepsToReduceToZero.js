76be2ec8c6210770482ed74a3fa960b926a882fe
Quinn Royston <quinn.royston@gmail.com>
2020-10-08 08:37:54 -0700

    2nd solutino for number of steps to reduce to zero

--- easy/numberOfStepsToReduceToZero.js
+++ easy/numberOfStepsToReduceToZero.js
@@ -19,3 +19,26 @@ var numberOfSteps  = function(num) {
 //increment count
 
 //return count + numberOfSteps invoked on the new number
+
+//solution 2
+var numberOfSteps  = function(num) {
+  var count = 0;
+  while (num > 0) {
+      if(num % 2 === 0) {
+          num = num/2;
+      } else {
+          num -= 1;
+      }
+      count++;
+  }
+  return count;
+};
+
+//count var
+
+//while num > 0;
+//if num is even
+  //divide by two
+//else decrement by 1
+//increment count;
+//return count;
