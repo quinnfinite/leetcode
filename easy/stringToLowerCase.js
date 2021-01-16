1f5d2de9cc1a312f372405250a9afaa0a9f43416
Quinn Royston <quinn.royston@gmail.com>
2021-01-15 21:59:19 -0800

    Added 2nd solution for stringToLowerCase

--- easy/stringToLowerCase.js
+++ easy/stringToLowerCase.js
@@ -1,4 +1,44 @@
 // https://leetcode.com/problems/to-lower-case/submissions/
+// var toLowerCase = function(str) {
+//   return str.toLowerCase()
+// };
+
+//solution 2 - https://leetcode.com/problems/to-lower-case/submissions/
+const letters = {
+  'A': 'a',
+  'B': 'b',
+  'C' : 'c',
+  'D' : 'd',
+  'E' : 'e',
+  'F' : 'f',
+  'G' : 'g',
+  'H' : 'h',
+  'I' : 'i',
+  'J' : 'j',
+  'K' : 'k',
+  'L' : 'l',
+  'M' : 'm',
+  'N' : 'n',
+  'O' : 'o',
+  'P' : 'p',
+  'Q' : 'q',
+  'R' : 'r',
+  'S' : 's',
+  'T' : 't',
+  'U' : 'u',
+  'V' : 'v',
+  'W' : 'w',
+  'X' : 'x',
+  'Y' : 'y',
+  'Z' : 'z',
+}
 var toLowerCase = function(str) {
-  return str.toLowerCase()
+  let generatedString = '';
+  for (const char of str) {
+    generatedString += letters[char] ? letters[char] : char
+  }
+  return generatedString
 };
+
+
+console.log('lower case of APplE - ', toLowerCase('APplE'))
